901e34a82179daab1d35b0d52eb5bdb94b41d33e
Cesar Montaño <cesarm2833@gmail.com>
2025-06-21 15:11:13 -0600

    Agregar componentes de Footer, NavBar y Card al proyecto

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="navbar bg-base-300">
+    <!-- Inicio de la barra (Responsive dropdown) -->
+    <div class="navbar-start">
+      <!-- <div class="dropdown">
+        <div
+          tabindex="0"
+          role="button"
+          class="btn btn-ghost lg:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </div>
+        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li>
+            <NuxtLink
+              to="/main"
+              class="btn-ghost"
+            >
+              Inicio
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              to="/sign-in"
+              class="btn-ghost"
+            >
+              Iniciar sesión / Registro
+            </NuxtLink>
+          </li>
+        </ul>
+      </div> -->
+
+      <!-- Logo y título -->
+      <NuxtLink
+        to="/"
+        class="btn btn-ghost text-xl text font-bold"
+      >
+        Holaaa
+      </NuxtLink>
+    </div>
+
+    <!-- Opciones del menú para pantallas grandes -->
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <NuxtLink
+            to="/main"
+            class="btn-ghost"
+          >
+            Inicio
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/about"
+            class="btn-ghost"
+          >
+            Acerca de
+          </NuxtLink>
+        </li>
+
+        <ChatDialogflow />
+      </ul>
+    </div>
+
+    <!-- Botones finales: cambio de tema y autenticación -->
+    <div class="navbar-end flex gap-2">
+      <ClientOnly>
+        <ThemeChanger />
+      </ClientOnly>
+      <NuxtLink
+        to="/sign-in"
+        class="btn btn-primary btn-sm"
+      >
+        Iniciar sesión / Registro
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ frontend/components/card.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card bg-base-300 h-fit shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title">
+        <slot name="title" />
+      </h2>
+      <div class="divider my-0" />
+      <slot name="content" />
+    </div>
+  </div>
+</template>
